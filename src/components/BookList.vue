760990250680e042b5f6fc592d514baf8580f0d4
Cuong Nguyen <cduynguyen95@gmail.com>
2018-10-19 22:54:52 -0400

    First module

---
+++ src/components/BookList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>{{ title }}</div>
+</template>
+
+<script> 
+export default {
+    name: 'BookList',
+    data() {
+        return {title: 'All Books'}
+    }
+}
+</script>
+
+<style>
+h1, h2 {
+    font-weight: 400
+    }
+</style>
